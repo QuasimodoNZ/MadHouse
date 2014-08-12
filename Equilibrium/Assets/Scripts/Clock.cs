c01893042a36f8c87c52ffca46e40db902c4ade1
Peter <peter.is.brainy@gmail.com>
2014-08-12 15:18:10 +1200

    added clock button and test

---
+++ Equilibrium/Assets/Scripts/Clock.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+using System;
+
+public class Clock : MonoBehaviour {
+
+	private int count = 0;
+	public GUIText text;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnEnable ()
+	{
+		GetComponent<PressGesture> ().Pressed += pressedHandler;
+		GetComponent<ReleaseGesture> ().Released += releasedHandler;
+	}
+	
+	private void OnDisable ()
+	{
+		GetComponent<PressGesture> ().Pressed -= pressedHandler;
+		GetComponent<ReleaseGesture> ().Released -= releasedHandler;
+	}
+
+	private void releasedHandler (object sender, EventArgs e)
+	{
+		Debug.LogWarning ("HELP!!!");
+		nextTurn ();
+		text.text = Convert.ToString (count);
+	}
+
+	private void pressedHandler (object sender, EventArgs e)
+	{
+		Debug.LogWarning("do I work?");
+	}
+
+	public int getCount(){
+		return count;
+	}
+
+	public void nextTurn(){
+		count++;
+	}
+}
