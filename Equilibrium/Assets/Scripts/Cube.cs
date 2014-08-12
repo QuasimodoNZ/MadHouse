526ed24fecdcec073ef9ec24208c055b636930b0
Somfin <micr0softener@gmail.com>
2014-08-12 12:18:48 +1200

    Merging edits from snapping to current version. Snapping works in 3D space now. Should be good to go for collision detection.

--- Equilibrium/Assets/Scripts/Cube.cs
+++ Equilibrium/Assets/Scripts/Cube.cs
@@ -4,44 +4,61 @@ using TouchScript.Gestures;
 
 public class Cube : MonoBehaviour
 {
-		public int stance = 0;
+	public enum BuildingState {
+		Menu,
+		Pallet,
+		Dragging,
+		Built,
+		Selected
+	}
+	public BuildingState state = BuildingState.Pallet;
 
-		private void OnEnable ()
-		{
-				GetComponent<PressGesture> ().Pressed += pressedHandler;
-				GetComponent<ReleaseGesture> ().Released += releasedHandler;
+	private void OnEnable ()
+	{
+		GetComponent<PressGesture> ().Pressed += pressedHandler;
+		GetComponent<ReleaseGesture> ().Released += releasedHandler;
+	}
+
+	private void OnDisable ()
+	{
+		GetComponent<PressGesture> ().Pressed -= pressedHandler;
+		GetComponent<ReleaseGesture> ().Released -= releasedHandler;
+	}
+
+	private  int RoundOff (float i)
+	{
+		return ((int)Math.Round (i / 1.0)) * 1;
+	}
+
+	private void releasedHandler (object sender, EventArgs e)
+	{
+		if (state == BuildingState.Dragging) {
+			Destroy (GetComponent<PanGesture> ());
+			Vector3 pos = gameObject.transform.position;
+			pos.x = RoundOff (pos.x);
+			pos.y = RoundOff (pos.y);
+			pos.z = 0.0f;
+			gameObject.transform.position = pos;
+			state = BuildingState.Built;
 		}
-
-		private void OnDisable ()
-		{
-				GetComponent<PressGesture> ().Pressed -= pressedHandler;
-				GetComponent<ReleaseGesture> ().Released -= releasedHandler;
-		}
-
-		private  int RoundOff (float i)
-		{
-				return ((int)Math.Round (i / 1.0)) * 1;
-		}
-
-		private void releasedHandler (object sender, EventArgs e)
-		{
-				Destroy (GetComponent<PanGesture> ());
-			
-				Vector3 pos = gameObject.transform.position;
-				pos.x = RoundOff (pos.x);
-				pos.y = RoundOff (pos.y);
-
-				gameObject.transform.position = pos;
-		}
+	}
 	
-		private void pressedHandler (object sender, EventArgs e)
-		{
-				if (stance == 1)
-						return;
-				var obj = Instantiate (gameObject) as GameObject;
-				obj.name = (gameObject.name);
-				//gameObject.name = "placed" + gameObject.name;
-				stance++;
+	private void pressedHandler (object sender, EventArgs e)
+	{
+		if (state == BuildingState.Built) {
+			GameObject.Destroy(gameObject);
+			return;
 		}
-
+		var obj = Instantiate (gameObject) as GameObject;
+		obj.name = (gameObject.name);
+		obj.GetComponent<Cube> ().enabled = true;
+		obj.GetComponent<TapGesture> ().enabled = true;
+		obj.transform.parent = gameObject.transform.parent;
+		var pos = obj.transform.position;
+		pos.z = -3.0f;
+		gameObject.transform.position = pos;
+		//gameObject.name = "placed" + gameObject.name;
+		state = BuildingState.Dragging;
+	}
+	
 }
