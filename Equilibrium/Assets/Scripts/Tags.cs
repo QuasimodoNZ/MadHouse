25598670b4af619f3c9b1c78f46e81f3ebb6bc66
Peter <peter.is.brainy@gmail.com>
2014-08-19 10:50:09 +1200

    added tags script for managing tags

---
+++ Equilibrium/Assets/Scripts/Tags.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour {
+
+	public static string Built = "Built";
+
+	public static string Respawn = "Respawn";
+
+	public static string Finish = "Finish";
+
+	public static string Player = "Player";
+
+	public static string GameController = "GameController";
+
+	public static string Pallet = "Pallet";
+
+	public static string Interface = "Interface";
+
+	public static string Placing = "Placing";
+
+	public static string SourceBuilding = "SourceBuilding";
+
+	public static string Resource = "Resource";
+
+	public static string UtilisedResource = "UtilisedResource";
+}
