9518f6c8b748616c2e324a9bb81ad8ec3da57aaa
Somfin <micr0softener@gmail.com>
2014-08-19 12:58:28 +1200

    Tags are in on my branch. System works as intended. Preliminary economic ticker is in place.

---
+++ Equilibrium/Assets/Scripts/Tags.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour {
+	
+	public const string Built = "Built";
+	
+	public const string Respawn = "Respawn";
+	
+	public const string Finish = "Finish";
+	
+	public const string Player = "Player";
+	
+	public const string GameController = "GameController";
+	
+	public const string Pallet = "Pallet";
+	
+	public const string Interface = "Interface";
+	
+	public const string Placing = "Placing";
+	
+	public const string Draggable = "Draggable";
+	
+	public const string Resource = "Resource";
+	
+	public const string UtilisedResource = "UtilisedResource";
+}
