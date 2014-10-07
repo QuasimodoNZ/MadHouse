23bb7d576b778514a87c2dc68bc747912d594a47
Benjamin Riddell <benjamin.riddell@hotmail.com>
2014-10-07 14:12:41 +1300

    Minor fix for the fading popup, replaced the placed holder

--- Equilibrium/Assets/Scripts/FadingPopup.cs
+++ Equilibrium/Assets/Scripts/FadingPopup.cs
@@ -4,44 +4,44 @@ using System.Collections;
 public class FadingPopup: MonoBehaviour
 {
 
-	public Color color = new Color (0.8f, 0.8f, 0f, 1.0f);
-	public float scroll = 0.05f;  // scrolling velocity
-	public float duration = 3f; // time to die
-	public float alpha;
+		public Color color = new Color (0.8f, 0.8f, 0f, 1.0f);
+		public float scroll = 0.05f;  // scrolling velocity
+		public float duration = 3f; // time to die
+		public float alpha;
 
 
-	// Use this for initialization
-	void Start ()
-	{
-		renderer.material.color = color; // set text color
-		alpha = 1f;
+		// Use this for initialization
+		void Start ()
+		{
+				renderer.material.color = color; // set text color
+				alpha = 1f;
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (alpha > 0) {
-			transform.position += new Vector3 (0, 0, 0);// scroll * Time.deltaTime); 
-			alpha -= Time.deltaTime / duration;
-			Color previous = renderer.material.color;
-			renderer.material.color = new Color (previous.r, previous.g, previous.b, alpha);      
-		} else {
-			Destroy (gameObject);
 		}
-	}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (alpha > 0) {
+						transform.position += new Vector3 (0, 0, 0);// scroll * Time.deltaTime); 
+						alpha -= Time.deltaTime / duration;
+						Color previous = renderer.material.color;
+						renderer.material.color = new Color (previous.r, previous.g, previous.b, alpha);      
+				} else {
+						Destroy (gameObject);
+				}
+		}
 
 
-	public static void SpawnPopup (string message, float x, float y, Color colour)
-	{
-		GameObject instance = Resources.Load <GameObject> ("Fading Popup");
-		x = Mathf.Clamp (x, -14.3f, 14.3f); // clamp position to screen to ensure
-		y = Mathf.Clamp (y, -7.2f, 7.2f);  // the string will be visible
-		float angle = Mathf.Atan2 (y, x) * 180 / Mathf.PI;
-		GameObject gui = Instantiate (instance, new Vector3 (x, y, 0), Quaternion.identity) as GameObject;
-		if (colour != null)
-			gui.renderer.material.color = colour;
-		gui.transform.Rotate (0, 0, angle + 90);
-		gui.GetComponent<TextMesh> ().text = "Message";
-	}
+		public static void SpawnPopup (string message, float x, float y, Color colour)
+		{
+				GameObject instance = Resources.Load <GameObject> ("Fading Popup");
+				x = Mathf.Clamp (x, -14.3f, 14.3f); // clamp position to screen to ensure
+				y = Mathf.Clamp (y, -7.2f, 7.2f);  // the string will be visible
+				float angle = Mathf.Atan2 (y, x) * 180 / Mathf.PI;
+				GameObject gui = Instantiate (instance, new Vector3 (x, y, 0), Quaternion.identity) as GameObject;
+				if (colour != null)
+						gui.renderer.material.color = colour;
+				gui.transform.Rotate (0, 0, angle + 90);
+				gui.GetComponent<TextMesh> ().text = message;
+		}
 }
